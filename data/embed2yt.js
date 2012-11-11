8ee77aab54e4815bff118a8f33e265313c0e68a6
Kevin Cox <kevincox.ca@gmail.com>
2012-11-10 20:41:34 -0500

    Cool new feaures.
     - Customizeable url:  Can set any link you want, on Android defaults to opening YouTube app, on all others defaults to a youtu.be link.
     - Made thumbnail quality prefrence actually do anything, although I don't know
        why anyone would use the regular res as the high is still pretty low (IMHO).
     - Passed in options using the `options` option on `page-mod`, didn't know that
        existed, those smart Mozilla people always putting in convient features.
     - Made the inital replace of the thumbnail faster, the title and image will
        pop up when they are ready.

--- data/embed2yt.js
+++ data/embed2yt.js
@@ -4,40 +4,38 @@ var framesel = document.getElementsByTagName("iframe");
 
 var frames = [];
 for ( i in framesel )         // When we removed the frames element list they
-    frames.push(framesel[i]); // disappear from the list causing index errors.
+	frames.push(framesel[i]); // disappear from the list causing index errors.
 
-self.port.on("gotData", function (data)
+for ( i in frames )
 {
-    for ( i in frames )
-    {
-        let f = frames[i];
-        var r = f.src.match(ytre);
-        if (r)
-        {
-            let ptl = r[1];
-            let www = r[2];
-            let vid = r[3];
-            
-            let tn = document.createElement("a");
-            tn.href = ptl+"youtu.be/"+vid;
-            let req = new XMLHttpRequest();
-            req.open('GET', 'https://gdata.youtube.com/feeds/api/videos/'+vid+'?v=2&alt=jsonc');
-            req.responseType = "json";
-            req.onload = function(e) {
-                //console.log(req.response);
-                tn.innerHTML = req.response.data.title;
-            
-                tn.className = "embed2yt";
-                tn.style.display = "block";
-                tn.style.width  = f.width + "px";
-                tn.style.height = f.height + "px";
-                tn.style.backgroundImage = "url("+data.playIcon+")," +
-                                           "url("+req.response.data.thumbnail.hqDefault+")";
-                
-                f.parentNode.insertBefore(tn, f);
-                f.parentNode.removeChild(f);
-            };
-            req.send();
-        }
-    }
-});
+	let f = frames[i];
+	var r = f.src.match(ytre);
+	if (r)
+	{
+		let ptl = r[1];
+		let www = r[2];
+		let vid = r[3];
+
+		let tn = document.createElement("a");
+		tn.href = self.options.pref.url.replace("%vid%", vid);
+
+		tn.className = "embed2yt";
+		tn.style.display = "block";
+		tn.style.width  = f.width + "px";
+		tn.style.height = f.height + "px";
+
+		f.parentNode.insertBefore(tn, f);
+		f.parentNode.removeChild(f);
+
+		let req = new XMLHttpRequest();
+		req.open('GET', 'https://gdata.youtube.com/feeds/api/videos/'+vid+'?v=2&alt=jsonc');
+		req.responseType = "json";
+		req.onload = function(e) {
+			tn.innerHTML = req.response.data.title;
+
+			tn.style.backgroundImage = "url("+self.options.playIcon+")," +
+			                           "url("+req.response.data.thumbnail[self.options.pref.thumbnailQuality]+")";
+		};
+		req.send();
+	}
+}
